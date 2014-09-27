c0fc4766fefd59da7fac7f6f6a28797b7fb896bc
Chris Johnson <cjohnson@rhythm.com>
2014-09-26 17:23:59 -0700

    Manifest Maven Plugin

---
+++ plugin/src/main/java/com/rhythm/louie/manifest/ManifestMojo.java
@@ -0,0 +1,102 @@
+/*
+ * ManifestMojo.java
+ * 
+ * Copyright (c) 2014 Rhythm & Hues Studios. All rights reserved.
+ */
+package com.rhythm.louie.manifest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Update the manifest with the Build Version and Time
+ * 
+ * @author cjohnson
+ */
+@Mojo(name = "manifest", defaultPhase = LifecyclePhase.PROCESS_CLASSES)
+public class ManifestMojo extends AbstractMojo {
+    /**
+      * The current project representation.
+      */
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
+    /**
+     * The format of the timestamp to store
+     */
+    @Parameter(property="manifest.dateformat", defaultValue = "HH:mm:ss MM/dd/yyyy", required = true)
+    private String dateformat;
+    
+    /**
+     * The process to run to retrieve the version string
+     */
+    @Parameter(property="manifest.versionexec", defaultValue = "git describe --long --always")
+    private String versionexec;
+    
+    @Override
+    public void execute() throws MojoExecutionException {
+        getLog().info("Gathering Build Properties");
+        
+        // store the current time                       
+        String currentBuildTime = new SimpleDateFormat(dateformat).format(new Date());
+        project.getProperties().put("build.time", currentBuildTime); 
+        
+        // retrieve the version through the exec
+        String versionText = null;
+        try {
+            versionText = retrieveVersion();
+        } catch (IOException e) {
+            getLog().error(e.toString());
+        }
+        
+        if (versionText == null || versionText.isEmpty()) {
+            versionText = "Unknown";
+        }
+        project.getProperties().put("build.version", versionText);
+    }
+    
+    private String retrieveVersion() throws IOException {
+        getLog().info("Retrieving version through: "+versionexec);
+        
+        Process p = Runtime.getRuntime().exec(versionexec);
+        
+        try (BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+             BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))
+            ) {
+            String line;
+            
+            //ERROR HANDLING
+            StringBuilder errorText = new StringBuilder();
+            while ((line = error.readLine()) != null) {
+                if (errorText.length()!=0) {
+                    errorText.append(" ");
+                }
+                errorText.append(line);
+            }
+            if (errorText.length()!=0) {
+                getLog().warn(errorText);
+            }
+            
+            //INPUT HANDLING
+            StringBuilder version = new StringBuilder();
+            while ((line = input.readLine()) != null) {
+                if (version.length()!=0) {
+                    version.append(" ");
+                }
+                version.append(line);
+            }
+            return version.toString();
+        }
+    }
+}
